01a8b90fd75c4d81669c38745c019a5b8743c842
Ryan <ryanthomas1998@gmail.com>
2020-03-16 10:47:18 -0400

    add another fix attempt

---
+++ sqlova/install_depedencies.sh
@@ -0,0 +1,7 @@
+# set noninteractive installation
+export DEBIAN_FRONTEND=noninteractive
+#install tzdata package
+apt install -y postgresql python-psycopg2 libpq-dev
+# set your timezone
+ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
+dpkg-reconfigure --frontend noninteractive tzdata
